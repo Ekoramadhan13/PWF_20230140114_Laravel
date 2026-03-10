58f88b2cedc6c95db1cb8310dec7637b7c334819
Ekoramadhan13 <enugraha411@gmail.com>
2026-03-10 21:21:57 +0700

    membuat file product di folder model dan migration php artisan make:model Produk -m

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+        'qty',
+        'price'
+    ];
+
+    // Relasi ke User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke Kategori
+    public function kategoris()
+    {
+        return $this->hasMany(Kategori::class);
+    }
+}
